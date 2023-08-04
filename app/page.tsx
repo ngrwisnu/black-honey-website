1bd4f9da1ccc282dfd454fe11dfc3abe1df9c1f9
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-08-04 15:27:32 +0700

    feat(cart): create the step component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
+import Step from "@/components/ui/cart/step";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between px-4 py-14">
-        <Button>Click me</Button>
+      <main className="flex min-h-screen flex-col items-center px-4 py-14">
+        <div className="flex">
+          <Step variant="disabled" label="1" />
+          <Step variant="active" label="1" />
+          <Step variant="done" />
+        </div>
       </main>
     </>
   );
