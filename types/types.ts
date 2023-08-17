fcbe6c1f3789d996af174b29622fd76bffcd6195
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-08-17 15:29:39 +0700

    chore(types): update the types file

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,20 @@
 export interface ProductType {
-  id: number;
+  id: string;
   name: string;
+  size: number;
+  stock: number;
   price: number;
+  status: string;
+  thumbnail: string;
+}
+
+export interface PaymentType {
+  id: number;
+  type: string;
+  payment_name: string;
+  account_number: number;
+  recipient_name: string;
+  status: string;
   thumbnail: string;
 }
 
@@ -12,4 +25,22 @@ export interface OrderType {
   status: string;
   product: ProductType;
   createdAt: string;
+}
+
+export interface AddressType {
+  id: string;
+  city: string;
+  province: string;
+  full_address: string;
+  recipient_name: string;
+  phone: string;
+}
+
+export interface UserType {
+  id: string;
+  username: string;
+  email: string;
+  role: string;
+  avatar: string;
+  addresses: AddressType[];
 }
