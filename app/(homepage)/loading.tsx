a8333eb4d3fd7043dbeb75f2b49f0fc7ff49a7cd
ngrwisnu <adiprayogongrwisnu@gmail.com>
2025-01-06 10:11:13 +0700

    [update] add custom loading component

--- app/(homepage)/loading.tsx
+++ app/(homepage)/loading.tsx
@@ -1,13 +1,8 @@
+import CustomLoading from "@/components/ui/loading";
 import React from "react";
 
 const Loading = () => {
-  return (
-    <div className="absolute inset-0 z-[999] flex items-center justify-center rounded">
-      <div className="flex h-20 w-20 justify-center bg-white p-4 shadow-md">
-        <span className="h-8 w-8 animate-spin rounded-full border-4 border-orange-200 border-t-[#CF933F]"></span>
-      </div>
-    </div>
-  );
+  return <CustomLoading />;
 };
 
 export default Loading;
